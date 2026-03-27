fbfdf80c0d41d638a39cec4687ecbee223146134
Nitendra mishra <nitendramishra788@gmail.com>
2026-03-27 22:15:19 +0530

    support section done

--- frontend/src/landing_Page/support/Hero.js
+++ frontend/src/landing_Page/support/Hero.js
@@ -3,8 +3,34 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div style={{backgroundColor:"#387ed1"}}>
+       <div className='container'>
+        <div className='row'>
+            <div className='col' style={{color:"white"}}>Support Portal</div>
+            <div className='col' style={{color:"white"}}>Track Tickets
+                <h6>Featured</h6>
+                <p style={{textDecoration:"underline"}}>1. Current Takeovers and Delisting – January 2024</p>
+
+                 <p style={{textDecoration:"underline"}}>2. Latest Intraday leverages – MIS & CO</p>
+            </div>
+        </div>
+
+        <div className='row'>
+            <div className='col'>
+                <h6 style={{color:"white"}}>Search for an answer or browse help topics to create a ticket </h6>
+                <input placeholder='Eg: how do i activate F&O, why is my order getting rejected..' style={{width:"100%" , height:"50px" , borderRadius:"1rem" }}></input>
+
+                <p><a href='#' style={{color:"white"}}>Track account opening</a> <a href='#' style={{color:"white"}}>Track segment activation</a> <a href='#' style={{color:"white"}}>Intraday </a></p>
+              
+            </div>
+            <div className='col'></div>
+        </div>
+       </div>
+
+       </div>
      );
+
+     
 }
 
 export default Hero;
--- frontend/src/landing_Page/support/Ticket.js
+++ frontend/src/landing_Page/support/Ticket.js
@@ -4,7 +4,87 @@ import React from 'react'
 function Ticket() {
     return ( 
 
-        <h1>Create Ticket</h1>
+        <div className='container'>
+            <div className='row'>
+                <h3 className='p-5'>To create a tickket, select relevant topic</h3>
+                <div className='col'><h6 ><i class="fa-solid fa-plus"></i>Account Opening</h6>
+                 
+                  <ul style={{listStyle:"none" , lineHeight:"40px"}}>
+                    <li><a href="#">Online Account Opening</a></li>
+                    <li><a href="#">Offline Account Opening</a></li>
+                    <li><a href="#">Company, Partnership and HUF Account Opening</a></li>
+                    <li><a href="#">NRI Account Opening</a></li>
+                    <li><a href="#">Charges at Zerodha</a></li>
+                    <li><a href="#">Zerodha IDFC FIRST Bank 3-in-1 Account</a></li>
+                    <li><a href="#">Getting Started</a></li>
+                    </ul>                   
+                </div>
+                 <div className='col'><h6><i class="fa-solid fa-user"></i>Your Zerodha Account</h6>
+
+                    <ul style={{listStyle:"none" , lineHeight:"40px"}}>
+                    <li><a href="#">Login Credentials</a></li>
+                    <li><a href="#">Account Modification and Segment Addition</a></li>
+                    <li><a href="#">DP ID and bank details</a></li>
+                    <li><a href="#">Your Profile</a></li>
+                    <li><a href="#">Transfer and conversion of shares</a></li>
+                </ul>               
+                 
+                 </div>
+                  <div className='col'><h6><i class="fa-solid fa-chart-column"></i>Your Zerodha Account</h6>
+                  
+                  <ul style={{listStyle:"none" , lineHeight:"40px"}}>
+                    <li><a href="#">Margin/leverage, Product and Order types</a></li>
+                    <li><a href="#">Kite Web and Mobile</a></li>
+                    <li><a href="#">Trading FAQs</a></li>
+                    <li><a href="#">Corporate Actions</a></li>
+                    <li><a href="#">Sentinel</a></li>
+                    <li><a href="#">Kite API</a></li>
+                    <li><a href="#">Pi and other platforms</a></li>
+                    <li><a href="#">Stockreports+</a></li>
+                    <li><a href="#">GTT</a></li>
+                    </ul>                   
+                  
+                  </div>
+            </div>
+
+
+            <div className='row p-4'>
+                <div className='col'><h6><i class="fa-regular fa-credit-card"></i>Funds</h6>
+                <ul style={{listStyle:"none" , lineHeight:"40px"}}>
+                <li><a href="#">Adding Funds</a></li>
+                <li><a href="#">Fund Withdrawal</a></li>
+                <li><a href="#">eMandates</a></li>
+                <li><a href="#">Adding Bank Accounts</a></li>
+                </ul>
+
+                </div>
+                <div className='col'><h6><i class="fa-regular fa-circle"></i>Console</h6>
+                    <ul style={{listStyle:"none" , lineHeight:"40px"}}>
+                        <li><a href="#">Reports</a></li>
+                        <li><a href="#">Ledger</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">60 Day Challenge</a></li>
+                        <li><a href="#">IPO</a></li>
+                        <li><a href="#">Referral Program</a></li>
+                        </ul>
+                
+                </div>
+                <div className='col'><h6><i class="fa-regular fa-circle"></i>Coin</h6>
+                
+               <ul style={{listStyle:"none" , lineHeight:"40px"}}>
+                    <li><a href="#">Understanding Mutual Funds</a></li>
+                    <li><a href="#">About Coin</a></li>
+                    <li><a href="#">Buying and Selling MF</a></li>
+                    <li><a href="#">Starting an SIP</a></li>
+                    <li><a href="#">Managing your Portfolio</a></li>
+                    <li><a href="#">Coin App</a></li>
+                    <li><a href="#">Moving to Coin</a></li>
+                    <li><a href="#">Government Securities</a></li>
+                    </ul>
+                
+                </div>
+            </div>
+        </div>
      );
 }
 
